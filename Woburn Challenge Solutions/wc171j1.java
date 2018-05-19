8c8d79a902156c6c6a297d4f3a64577e9e909b8b
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 18:33:49 -0400

    Create wc171j1.java

---
+++ Woburn Challenge Solutions/wc171j1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class wc171j1{
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		String a = scan.nextLine();
+		System.out.println(a+", eh");
+	}
+}
