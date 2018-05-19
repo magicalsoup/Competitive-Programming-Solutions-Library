7e7449bd5856f797450dbe0abddaa13b894697b4
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 18:37:29 -0400

    Create wc172j1.java

---
+++ Woburn Challenge Solutions/wc172j1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class wc172j1{
+     public static void main(String []args){
+    	 @SuppressWarnings("resource")
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = a+b+c+1;
+        System.out.println(d);
+         
+     }
+}
