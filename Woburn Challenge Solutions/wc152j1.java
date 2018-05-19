7f10156382470d08ca2c754e5c74924968a2e6ab
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 16:34:10 -0400

    Create wc152j1.java

---
+++ Woburn Challenge Solutions/wc152j1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class wc152j1 {
+	public static void main (String[] args){
+		@SuppressWarnings("resource")
+    	Scanner scan = new Scanner(System.in);
+int t = scan.nextInt();
+System.out.print("A long time ago in a galaxy ");
+
+for(int i = 0;i<t-1;i++) {
+	System.out.print("far, ");
+}
+	
+	System.out.print("far away...");
+	
+	
+	}
+	}
