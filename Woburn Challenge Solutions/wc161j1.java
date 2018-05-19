08137cd0e1502c81978f5bd23db6117e4a5c136b
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 16:45:57 -0400

    Create wc161j1.java

---
+++ Woburn Challenge Solutions/wc161j1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class wc161j1{
+	public static void main (String[] args){
+		@SuppressWarnings("resource")
+    	Scanner scan = new Scanner(System.in);
+int t = scan.nextInt();
+System.out.print("sp");
+
+for(int i = 0;i<t-1;i++) {
+	System.out.print("o");
+}
+	
+	System.out.print("oky");
+	
+	
+	}
+}
