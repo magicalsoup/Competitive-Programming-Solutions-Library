4a78ac276c6c9a7e715697c0b9f7ec4d85448bae
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 16:01:44 -0400

    Create cerc07b.java

---
+++ Others and Challenges Wcipeg/cerc07b.java
@@ -0,0 +1,83 @@
+import java.util.*;
+public class cerc07b {
+static boolean[]firstrow = new boolean[16];
+static int mintaps = -1;
+public static void main(String[]args) {
+	@SuppressWarnings("resource")
+	Scanner sc = new Scanner(System.in);
+	int r = sc.nextInt();
+	int c = sc.nextInt();
+	boolean[][] tiles = new boolean[r][c];
+	for(int i=0;i<r;i++)
+	{
+		String line = sc.next();
+		for(int j=0;j<c;j++)
+		{
+			if(line.charAt(j)=='X')
+				tiles[i][j] = true;
+			else
+				tiles[i][j] = false;
+		}
+	}
+	findpos(tiles,0);
+	if(mintaps==-1)
+		System.out.println("Damaged billboard.");
+	else
+		System.out.printf("You have to tap %s tiles.\n",mintaps);
+}
+static void findpos(boolean[][]startingtiles,int pos)
+{
+	if(pos==startingtiles[0].length)
+	{
+		int taps = 0;
+		boolean [][]tiles = new boolean[startingtiles.length][startingtiles[0].length];
+		for(int r=0;r<tiles.length;r++)
+			for(int c=0;c<tiles[0].length;c++)
+				tiles[r][c] = startingtiles[r][c];
+		
+		for(int c=0;c<tiles[0].length;c++)
+			if(firstrow[c]==true)
+			{
+				tiles[0][c]= !tiles[0][c];
+				if(tiles.length>1)
+					tiles[1][c] = !tiles[1][c];
+				if(c>0)
+					tiles[0][c-1] = !tiles[0][c-1];
+				if(c<tiles[0].length-1)
+					tiles[0][c+1] = !tiles[0][c+1];
+				taps++;
+			}
+		checkgrid(tiles,taps);
+		return;
+	}
+	findpos(startingtiles,pos+1);
+	firstrow[pos] = true;
+	findpos(startingtiles,pos+1);
+	firstrow[pos] = false;
+}
+static void checkgrid(boolean[][]tiles,int taps) {
+	for(int r=1;r<tiles.length;r++)
+		for(int c=0;c<tiles[0].length;c++)
+			if(tiles[r-1][c]==true)
+			{
+				tiles[r-1][c] = !tiles[r-1][c];
+				taps++;
+				tiles[r][c] = !tiles[r][c];
+				if(c>0)
+					tiles[r][c-1] = !tiles[r][c-1];
+				if(c<tiles[0].length-1)
+					tiles[r][c+1] = !tiles[r][c+1];
+				if(r<tiles.length-1)
+					tiles[r+1][c] = !tiles[r+1][c];
+			}
+	for(int r=0;r<tiles.length;r++)
+		for(int c=0;c<tiles[0].length;c++)
+			if(tiles[r][c]==true)
+			{
+				taps = -1;
+				break;
+			}
+	if((taps<mintaps||mintaps==-1)&&taps!=-1)
+		mintaps = taps;
+}
+}
