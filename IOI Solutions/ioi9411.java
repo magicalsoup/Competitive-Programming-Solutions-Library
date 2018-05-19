ee0e2da18a92d839a94230485dac8e0433765565
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 19:14:04 -0400

    Create ioi9411.java

---
+++ IOI Solutions/ioi9411.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ioi9411{
+ static Scanner scan = new Scanner(System.in);
+ public static void main(String[]args) {
+	 int rows = scan.nextInt();
+	 int[][] triangle = new int[rows][];
+	 for(int x =0;x<triangle.length;x++) {
+		 triangle[x] = new int[x+1];
+		 for(int y =0;y< triangle[x].length;y++)
+			 triangle[x][y] = scan.nextInt();
+	 }
+	 for(int x= triangle.length-2;x>=0;x--)
+		 for(int y=0;y<triangle[x].length;y++)
+			 triangle[x][y] += Math.max(triangle[x+1][y], triangle[x+1][y+1]);
+	 System.out.println(triangle[0][0]);
+ }
+}
