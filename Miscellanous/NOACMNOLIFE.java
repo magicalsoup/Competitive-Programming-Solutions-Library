bd6c3f29b22cc17d5585aa2222efaa68e5ecd1c0
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 21:46:11 -0400

    Create NOACMNOLIFE.java

---
+++ Miscellanous/NOACMNOLIFE.java
@@ -0,0 +1,603 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+import java.util.Stack;
+
+public class NOACMNOLIFE {
+	private static NOACMNOLIFE o = new NOACMNOLIFE();
+	public class Reader {
+		private BufferedReader in;
+		private StringTokenizer st;
+		
+		public Reader(InputStream inputStream) {
+			in = new BufferedReader(new InputStreamReader(inputStream));
+		} // Reader InputStream constructor
+		
+		public Reader(String fileName) throws FileNotFoundException {
+			in = new BufferedReader(new FileReader(fileName));
+		} // Reader String constructor
+
+		public String next() throws IOException {
+			while (st == null || !st.hasMoreTokens()) {
+				st = new StringTokenizer(in.readLine().trim());
+			} // while
+			return st.nextToken();
+		} // next method
+		
+		public long nextLong() throws IOException {
+			return Long.parseLong(next());
+		} // nextLong method
+		
+		public int nextInt() throws IOException {
+			return Integer.parseInt(next());
+		} // nextInt method
+		
+		public double nextDouble() throws IOException {
+			return Double.parseDouble(next());
+		} // nextDouble method
+		
+		public String nextLine() throws IOException {
+			return in.readLine().trim();
+		} // nextLine method
+	} // Reader class
+	
+	public class Bag<Item> implements Iterable<Item> {
+	    private Node<Item> first;    // beginning of bag
+	    private int n;               // number of elements in bag
+
+	    // helper linked list class
+	    @SuppressWarnings("hiding")
+		private class Node<Item> {
+	        private Item item;
+	        private Node<Item> next;
+	    }
+
+	    /**
+	     * Initializes an empty bag.
+	     */
+	    public Bag() {
+	        first = null;
+	        n = 0;
+	    }
+
+	    /**
+	     * Returns true if this bag is empty.
+	     *
+	     * @return {@code true} if this bag is empty;
+	     *         {@code false} otherwise
+	     */
+	    public boolean isEmpty() {
+	        return first == null;
+	    }
+
+	    /**
+	     * Returns the number of items in this bag.
+	     *
+	     * @return the number of items in this bag
+	     */
+	    public int size() {
+	        return n;
+	    }
+
+	    /**
+	     * Adds the item to this bag.
+	     *
+	     * @param  item the item to add to this bag
+	     */
+	    public void add(Item item) {
+	        Node<Item> oldfirst = first;
+	        first = new Node<Item>();
+	        first.item = item;
+	        first.next = oldfirst;
+	        n++;
+	    }
+
+
+	    /**
+	     * Returns an iterator that iterates over the items in this bag in arbitrary order.
+	     *
+	     * @return an iterator that iterates over the items in this bag in arbitrary order
+	     */
+	    public Iterator<Item> iterator()  {
+	        return new ListIterator<Item>(first);  
+	    }
+
+	    // an iterator, doesn't implement remove() since it's optional
+	    @SuppressWarnings("hiding")
+		private class ListIterator<Item> implements Iterator<Item> {
+	        private Node<Item> current;
+
+	        public ListIterator(Node<Item> first) {
+	            current = first;
+	        }
+
+	        public boolean hasNext()  { return current != null;                     }
+	        public void remove()      { throw new UnsupportedOperationException();  }
+
+	        public Item next() {
+	            if (!hasNext()) throw new NoSuchElementException();
+	            Item item = current.item;
+	            current = current.next; 
+	            return item;
+	        }
+	    }
+	}
+	
+	public class Queue<Item> implements Iterable<Item> {
+	    private Node<Item> first;    // beginning of queue
+	    private Node<Item> last;     // end of queue
+	    private int n;               // number of elements on queue
+
+	    // helper linked list class
+	    @SuppressWarnings("hiding")
+		private class Node<Item> {
+	        private Item item;
+	        private Node<Item> next;
+	    }
+
+	    /**
+	     * Initializes an empty queue.
+	     */
+	    public Queue() {
+	        first = null;
+	        last  = null;
+	        n = 0;
+	    }
+
+	    /**
+	     * Returns true if this queue is empty.
+	     *
+	     * @return {@code true} if this queue is empty; {@code false} otherwise
+	     */
+	    public boolean isEmpty() {
+	        return first == null;
+	    }
+
+	    /**
+	     * Returns the number of items in this queue.
+	     *
+	     * @return the number of items in this queue
+	     */
+	    public int size() {
+	        return n;
+	    }
+
+	    /**
+	     * Returns the item least recently added to this queue.
+	     *
+	     * @return the item least recently added to this queue
+	     * @throws NoSuchElementException if this queue is empty
+	     */
+	    public Item peek() {
+	        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+	        return first.item;
+	    }
+
+	    /**
+	     * Adds the item to this queue.
+	     *
+	     * @param  item the item to add
+	     */
+	    public void enqueue(Item item) {
+	        Node<Item> oldlast = last;
+	        last = new Node<Item>();
+	        last.item = item;
+	        last.next = null;
+	        if (isEmpty()) first = last;
+	        else           oldlast.next = last;
+	        n++;
+	    }
+
+	    /**
+	     * Removes and returns the item on this queue that was least recently added.
+	     *
+	     * @return the item on this queue that was least recently added
+	     * @throws NoSuchElementException if this queue is empty
+	     */
+	    public Item dequeue() {
+	        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+	        Item item = first.item;
+	        first = first.next;
+	        n--;
+	        if (isEmpty()) last = null;   // to avoid loitering
+	        return item;
+	    }
+
+	    /**
+	     * Returns a string representation of this queue.
+	     *
+	     * @return the sequence of items in FIFO order, separated by spaces
+	     */
+	    public String toString() {
+	        StringBuilder s = new StringBuilder();
+	        for (Item item : this) {
+	            s.append(item);
+	            s.append(' ');
+	        }
+	        return s.toString();
+	    } 
+
+	    /**
+	     * Returns an iterator that iterates over the items in this queue in FIFO order.
+	     *
+	     * @return an iterator that iterates over the items in this queue in FIFO order
+	     */
+	    public Iterator<Item> iterator()  {
+	        return new ListIterator<Item>(first);  
+	    }
+
+	    // an iterator, doesn't implement remove() since it's optional
+	    @SuppressWarnings("hiding")
+		private class ListIterator<Item> implements Iterator<Item> {
+	        private Node<Item> current;
+
+	        public ListIterator(Node<Item> first) {
+	            current = first;
+	        }
+
+	        public boolean hasNext()  { return current != null;                     }
+	        public void remove()      { throw new UnsupportedOperationException();  }
+
+	        public Item next() {
+	            if (!hasNext()) throw new NoSuchElementException();
+	            Item item = current.item;
+	            current = current.next; 
+	            return item;
+	        }
+	    }
+	}
+	
+	public class UF {
+
+	    private int[] parent;  // parent[i] = parent of i
+	    private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
+	    private int count;     // number of components
+
+	    /**
+	     * Initializes an empty union–find data structure with {@code n} sites
+	     * {@code 0} through {@code n-1}. Each site is initially in its own 
+	     * component.
+	     *
+	     * @param  n the number of sites
+	     * @throws IllegalArgumentException if {@code n < 0}
+	     */
+	    public UF(int n) {
+	        if (n < 0) throw new IllegalArgumentException();
+	        count = n;
+	        parent = new int[n];
+	        rank = new byte[n];
+	        for (int i = 0; i < n; i++) {
+	            parent[i] = i;
+	            rank[i] = 0;
+	        }
+	    }
+
+	    /**
+	     * Returns the component identifier for the component containing site {@code p}.
+	     *
+	     * @param  p the integer representing one site
+	     * @return the component identifier for the component containing site {@code p}
+	     * @throws IndexOutOfBoundsException unless {@code 0 <= p < n}
+	     */
+	    public int find(int p) {
+	        validate(p);
+	        while (p != parent[p]) {
+	            parent[p] = parent[parent[p]];    // path compression by halving
+	            p = parent[p];
+	        }
+	        return p;
+	    }
+
+	    /**
+	     * Returns the number of components.
+	     *
+	     * @return the number of components (between {@code 1} and {@code n})
+	     */
+	    public int count() {
+	        return count;
+	    }
+	  
+	    /**
+	     * Returns true if the the two sites are in the same component.
+	     *
+	     * @param  p the integer representing one site
+	     * @param  q the integer representing the other site
+	     * @return {@code true} if the two sites {@code p} and {@code q} are in the same component;
+	     *         {@code false} otherwise
+	     * @throws IndexOutOfBoundsException unless
+	     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+	     */
+	    public boolean connected(int p, int q) {
+	        return find(p) == find(q);
+	    }
+	  
+	    /**
+	     * Merges the component containing site {@code p} with the 
+	     * the component containing site {@code q}.
+	     *
+	     * @param  p the integer representing one site
+	     * @param  q the integer representing the other site
+	     * @throws IndexOutOfBoundsException unless
+	     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
+	     */
+	    public void union(int p, int q) {
+	        int rootP = find(p);
+	        int rootQ = find(q);
+	        if (rootP == rootQ) return;
+
+	        // make root of smaller rank point to root of larger rank
+	        if      (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
+	        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
+	        else {
+	            parent[rootQ] = rootP;
+	            rank[rootP]++;
+	        }
+	        count--;
+	    }
+
+	    // validate that p is a valid index
+	    private void validate(int p) {
+	        int n = parent.length;
+	        if (p < 0 || p >= n) {
+	            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));  
+	        }
+	    }
+	}
+
+	public class Graph {
+	    private final String NEWLINE = System.getProperty("line.separator");
+
+	    private final int V;
+	    private int E;
+	    private Bag<Integer>[] adj;
+	    
+	    /**
+	     * Initializes an empty graph with {@code V} vertices and 0 edges.
+	     * param V the number of vertices
+	     *
+	     * @param  V number of vertices
+	     * @throws IllegalArgumentException if {@code V < 0}
+	     */
+	    @SuppressWarnings("unchecked")
+		public Graph(int V) {
+	        if (V < 0) throw new IllegalArgumentException("Number of vertices must be nonnegative");
+	        this.V = V;
+	        this.E = 0;
+	        adj = (Bag<Integer>[]) new Bag[V];
+	        for (int v = 0; v < V; v++) {
+	            adj[v] = new Bag<Integer>();
+	        }
+	    }
+
+	    /**
+	     * Initializes a new graph that is a deep copy of {@code G}.
+	     *
+	     * @param  G the graph to copy
+	     */
+	    public Graph(Graph G) {
+	        this(G.V());
+	        this.E = G.E();
+	        for (int v = 0; v < G.V(); v++) {
+	            // reverse so that adjacency list is in same order as original
+	            Stack<Integer> reverse = new Stack<Integer>();
+	            for (int w : G.adj[v]) {
+	                reverse.push(w);
+	            }
+	            for (int w : reverse) {
+	                adj[v].add(w);
+	            }
+	        }
+	    }
+
+	    /**
+	     * Returns the number of vertices in this graph.
+	     *
+	     * @return the number of vertices in this graph
+	     */
+	    public int V() {
+	        return V;
+	    }
+
+	    /**
+	     * Returns the number of edges in this graph.
+	     *
+	     * @return the number of edges in this graph
+	     */
+	    public int E() {
+	        return E;
+	    }
+
+	    // throw an IllegalArgumentException unless {@code 0 <= v < V}
+	    private void validateVertex(int v) {
+	        if (v < 0 || v >= V)
+	            throw new IllegalArgumentException("vertex " + v + " is not between 0 and " + (V-1));
+	    }
+
+	    /**
+	     * Adds the undirected edge v-w to this graph.
+	     *
+	     * @param  v one vertex in the edge
+	     * @param  w the other vertex in the edge
+	     * @throws IllegalArgumentException unless both {@code 0 <= v < V} and {@code 0 <= w < V}
+	     */
+	    public void addEdge(int v, int w) {
+	        validateVertex(v);
+	        validateVertex(w);
+	        E++;
+	        adj[v].add(w);
+	        adj[w].add(v);
+	    }
+
+
+	    /**
+	     * Returns the vertices adjacent to vertex {@code v}.
+	     *
+	     * @param  v the vertex
+	     * @return the vertices adjacent to vertex {@code v}, as an iterable
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public Iterable<Integer> adj(int v) {
+	        validateVertex(v);
+	        return adj[v];
+	    }
+
+	    /**
+	     * Returns the degree of vertex {@code v}.
+	     *
+	     * @param  v the vertex
+	     * @return the degree of vertex {@code v}
+	     * @throws IllegalArgumentException unless {@code 0 <= v < V}
+	     */
+	    public int degree(int v) {
+	        validateVertex(v);
+	        return adj[v].size();
+	    }
+
+
+	    /**
+	     * Returns a string representation of this graph.
+	     *
+	     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+	     *         followed by the <em>V</em> adjacency lists
+	     */
+	    public String toString() {
+	        StringBuilder s = new StringBuilder();
+	        s.append(V + " vertices, " + E + " edges " + NEWLINE);
+	        for (int v = 0; v < V; v++) {
+	            s.append(v + ": ");
+	            for (int w : adj[v]) {
+	                s.append(w + " ");
+	            }
+	            s.append(NEWLINE);
+	        }
+	        return s.toString();
+	    }
+	}
+	
+	private static Reader in = o.new Reader(System.in);
+	private static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+	private static Graph G;
+	private static int N;
+	private static int K;
+	private static int[] labels;
+	private static int[] curDist;
+	private static int[][] dist;
+	private static boolean[] marked;
+	private static HashMap<Integer, Integer> count;
+	private static int[][] countLabels;
+	private static double[][] comb;
+	
+	public static void main(String[] args) throws IOException {
+		int T = in.nextInt();
+		for (int i = 0; i < T; i++) {
+			run();
+		}
+		out.close();
+	}
+
+	public static void run() throws IOException {
+		N = in.nextInt();
+		K = in.nextInt();
+		labels = new int[N];
+		dist = new int[N][N];
+		count = new HashMap<Integer, Integer>();
+		countLabels = new int[N][N];
+		comb = new double[N][N];
+		double ans = 0;
+		for (int i = K; i <= N; i++) {
+			comb[i - 2][K - 2] = choose(i - 2, K - 2);
+		}
+		G = o.new Graph(N);
+		for (int i = 0; i < N - 1; i++) {
+			G.addEdge(in.nextInt() - 1, in.nextInt() - 1);
+		}
+		for (int i = 0; i < N; i++) {
+			labels[i] = in.nextInt();
+		}
+		for (int i = 0; i < N; i++) {
+			bfs(i);
+		}
+		for (int i = 0; i < N; i++) {
+			marked = new boolean[N];
+			dfs(i, i);
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = i + 1; j < N; j++) {
+				int labels = countLabels[i][j];
+				int diameter = dist[i][j];
+				int count = 2;
+				for (int k = 0; k < N; k++) {
+					if (k == i || k == j) continue;
+					if (dist[k][i] > diameter || dist[k][j] > diameter) continue;
+					if ((dist[k][i] == diameter && k < j) || (dist[k][j] == diameter && k < i)) continue;
+					count++;
+				}
+				if (count >= K) {
+					ans += labels * comb[count - 2][K - 2];
+				}
+			}
+		}
+		out.println(ans / choose(N, K));
+	}
+
+	private static double choose(int n, int k) {
+		double ans = 1;
+		for (int i = 0; i < k; i++) {
+			ans = ans * (n - i) / (i + 1);
+		}
+		return ans;
+	}
+
+	private static void bfs(int s) {
+		curDist = new int[N];
+		marked = new boolean[N];
+		Queue<Integer> q = o.new Queue<Integer>();
+		for (int v = 0; v < G.V(); v++)
+			curDist[v] = Integer.MAX_VALUE;
+		curDist[s] = 0;
+		marked[s] = true;
+		q.enqueue(s);
+
+		while (!q.isEmpty()) {
+			int v = q.dequeue();
+			for (int w : G.adj(v)) {
+				if (!marked[w]) {
+					curDist[w] = curDist[v] + 1;
+					marked[w] = true;
+					q.enqueue(w);
+				}
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			dist[s][i] = curDist[i];
+		}
+	}
+
+	private static void dfs(int v, int s) {
+		if (!count.containsKey(labels[v])) {
+			count.put(labels[v], 0);
+		}
+		count.put(labels[v], (count.get(labels[v]) + 1));
+		countLabels[s][v] = count.size();
+		marked[v] = true;
+		for (int w : G.adj(v)) {
+			if (!marked[w]) {
+				dfs(w, s);
+			}
+		}
+		if (count.get(labels[v]) == 1) {
+			count.remove(labels[v]);
+		} else {
+			count.put(labels[v], (count.get(labels[v]) - 1));
+		}
+	}
+}
