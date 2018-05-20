9e9bf7514023746947a8d5ebdd774f0bf53f770e
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 21:04:13 -0400

    Create aplusb.java

---
+++ Miscellanous/aplusb.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class aplusb {
+	public static void main (String[] args)
+    {
+		@SuppressWarnings("resource")
+    	Scanner sc = new Scanner(System.in);
+System.out.println(sc.nextInt()+sc.nextInt());
+     
+}
+}
