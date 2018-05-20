e7763df37b1c3368a2a2dcf01772a250a1f9b4b3
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 21:57:58 -0400

    Create coci062p1.java

---
+++ COCI  Solutions/coci062p1.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class coci062p1{
+	public static void main (String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+double a =scan.nextDouble();
+double b =scan.nextDouble();
+double s = (b*2) -a;
+int  t = (int)s;
+System.out.println(t);
+
+	
+	
+	
+	
+	}
+	}
