e5fe030347bc10968dfa52ff639d528df5a3805d
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 21:15:37 -0400

    Create dp1p1.java

---
+++ Miscellanous/dp1p1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class dp1p1{
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		
+		int m = scan.nextInt();
+		int arr[] = new int[m];
+		for(int i = 0; i < m; i++){
+			int n = scan.nextInt();
+			arr[i] = n;
+		}
+		int dp[] = new int[m];
+		dp[0] = arr[0];
+		dp[1] = arr[1];
+		dp[2] = Math.max(dp[0] + arr[2], dp[1]);
+		for(int i=3;i<m;i++)
+		{
+			dp[i] = Math.max(Math.max(dp[i-2], dp[i-3]) + arr[i],dp[i-1]);
+		}
+		System.out.println(dp[m-1]);
+	}
+}
