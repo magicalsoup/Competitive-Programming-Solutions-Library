550acfe8d42c2fdbad05384ca8bf5e6390c67114
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 21:15:57 -0400

    Create dp1p2.java

---
+++ Miscellanous/dp1p2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class 	 {
+public static void main(String[]args) {
+	@SuppressWarnings("resource")
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int coin[] = new int[n];
+	for(int i=0;i<n;i++)
+		coin[i] = sc.nextInt();
+	
+	int[][]dp = new int [n][n];
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i;j>=0;j--)
+		{
+			if(j==i)
+				dp[i][j] = coin[i];
+			else
+			{
+				for(int k=j;k<=i;k++)
+					dp[i][j]+=coin[k];
+				dp[i][j]-=dp[i][j+1]>dp[i-1][j]?dp[i-1][j]:dp[i][j+1];
+			}
+		}
+	}
+	System.out.println(dp[n-1][0]);
+}
+}
