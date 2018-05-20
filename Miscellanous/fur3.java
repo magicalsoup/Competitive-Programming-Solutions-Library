b80253a458f5ada46f57d07ea0415ae813713c28
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 21:24:05 -0400

    Create fur3.java

---
+++ Miscellanous/fur3.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class fur3 {
+public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+
+int n = scan.nextInt();
+int arr[] = new int [n];
+for(int i =0;i<n;i++) {
+	arr[i] = scan.nextInt();
+}
+Arrays.sort(arr);
+	
+
+double median;
+if (n % 2 == 0) {
+    median = ((double)arr[n/2] + (double)arr[n/2 - 1])/2;
+}
+else {
+    median = (double) arr[n/2];
+}
+System.out.println(median);
+
+
+}
+}
