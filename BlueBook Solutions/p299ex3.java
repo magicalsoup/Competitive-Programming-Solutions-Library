8109b55c540ce1a1e37a88ec2283d639bb375fbb
magicalsoup <39444003+magicalsoup@users.noreply.github.com>
2018-05-19 20:46:56 -0400

    Create p299ex3.java

---
+++ BlueBook Solutions/p299ex3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class p299ex3{
+public static void main(String[] args) {
+	@SuppressWarnings("resource")
+	Scanner scan = new Scanner(System.in);
+int a =scan.nextInt();
+for (int i = 0; i < a; i++) {
+	String str =scan.next();
+	System.out.println((str).toLowerCase());
+}
+}
+}
