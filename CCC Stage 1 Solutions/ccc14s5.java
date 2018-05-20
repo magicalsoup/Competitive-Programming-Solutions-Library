7d9a868357da686cc86a52054b98f25888a343fa
James <39444003+magicalsoup@users.noreply.github.com>
2018-05-20 13:52:14 -0400

    Update ccc14s5.java

--- CCC Stage 1 Solutions/ccc14s5.java
+++ CCC Stage 1 Solutions/ccc14s5.java
@@ -1,98 +1,67 @@
-//too slow, but may work after a few tries, it about 0.006 too slow
 import java.util.*;
-import java.io.*;
-public class ccc14s5 {
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
-	static StringTokenizer st;
-public static void main(String[]args)throws IOException{
-	int n = readInt();
-	int []x = new int[n+1];
-	int []y= new int [n+1];
-	for(int i=1;i<=n;i++)
-	{
-		x[i] = readInt();
-		y[i] = readInt();
-	}
-	Jump [] jumps = new Jump[n*(n+1)/2];
-	int k = 0;
-	for(int i=0;i<n;i++)
-		for(int j=i+1;j<=n;j++)
-			jumps[k++] = new Jump(i,j,(x[i]-x[j])*(x[i]-x[j])+(y[i]-y[j])*(y[i]-y[j]));
-	int [] most = new int[n+1];
-	int [] last = new int[n+1];
-	int [] lastdist = new int[n+1];
-	Arrays.sort(jumps);
-	for(int i=0;i<jumps.length;i++)
-	{
-		int a = jumps[i].a;
-		int b = jumps[i].b;
-		if(jumps[i].d>lastdist[a])
-		{
-			lastdist[a] = jumps[i].d;
-			last[a] = most[a];
-		}
-		if(jumps[i].d>lastdist[b])
-		{
-			lastdist[b] = jumps[i].d;
-			last[b] = most[b];
-		}
-		if(a==0)
-		{
-			if(last[b]+1>most[0])
-				most[0] = last[b]+1;
-		}
-		else
-		{
-			if(last[b]+1>most[a])
-				most[a] = last[b]+1;
-			if(last[a]+1>most[b])
-				most[b] = last[a]+1;
-		}
-	}
-		out.println(most[0]);
-		out.close();
-		return;
-}
-static String next () throws IOException {
-    while (st == null || !st.hasMoreTokens())
-      st = new StringTokenizer(br.readLine().trim());
-    return st.nextToken();
-  }
+public class ccc14s5{
+    public static void main(String[] args) throws Exception {
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
 
-  static long readLong () throws IOException {
-    return Long.parseLong(next());
-  }
+        if (N == 1) {
+            System.out.println(1);
+            System.exit(0);
+        }
 
-  static int readInt () throws IOException {
-    return Integer.parseInt(next());
-  }
+        final int n = N + 1;
+        final int[] xs = new int[n];
+        final int[] ys = new int[n];
 
-  static double readDouble () throws IOException {
-    return Double.parseDouble(next());
-  }
+        int p = -1, i, j, x, y, dx, dy;
+        final long[] pds = new long[n * (n - 1) / 2];
+        for (i = 1; i < n; i++) {
+            xs[i] = s.nextInt();
+            ys[i] = s.nextInt();
+        }
+        s.close();
 
-  static String readLine () throws IOException {
-    return br.readLine().trim();
-  }
-}
-class Jump implements Comparable<Jump>
-{
-	int a,b;
-	int d;
-	Jump(int A,int B,int dist)
-	{
-		a = A;
-		b = B;
-		d = dist;
-	}
-	public int compareTo(Jump j)
-	{
-		if(d<j.d)
-			return -1;
-		else if(d==j.d)
-			return 0;
-		else
-			return 1;
-	}
+        for (i = 0; i < n; i++) {
+            x = xs[i];
+            y = ys[i];
+
+            for (j = i + 1; j < n; j++) {
+                dx = x - xs[j];
+                dy = y - ys[j];
+
+                pds[++p] = ((long) (dx * dx + dy * dy) << 32) + (i << 16) + j;
+            }
+        }
+
+        Arrays.sort(pds);
+
+        final int[] best = new int[n];
+        final int[] previousBest = new int[n];
+        final int[] distance = new int[n];
+
+        for (long pd : pds) {
+            int d = (int) (pd >> 32);
+            i = ((int) pd) >> 16;
+            j = ((int) pd) & 0b111111111111;
+
+            if (d != distance[j]) {
+                distance[j] = d;
+                previousBest[j] = best[j];
+            }
+            if (d != distance[i]) {
+                distance[i] = d;
+                previousBest[i] = best[i];
+            }
+
+            if (i == 0) {
+                // the origin is special because the fox cannot revisit it
+                best[0] = Math.max(best[0], previousBest[j]);
+            } else {
+                best[j] = Math.max(best[j], previousBest[i] + 1);
+                best[i] = Math.max(best[i], previousBest[j] + 1);
+            }
+        }
+
+        System.out.println(best[0] + 1);
+    }
 }
