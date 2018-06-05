e4a676ecea38665f3f995480096375aeafc48663
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-04 20:44:40 -0400

    Update and rename Selectionsort.java to SelectionSort.java

--- Algorithms/SelectionSort.java
+++ Algorithms/SelectionSort.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Selectionsort {
+public class SelectionSort {
 public static void main(String[]args) {
 	Scanner sc = new Scanner(System.in);
 	int n = sc.nextInt();
