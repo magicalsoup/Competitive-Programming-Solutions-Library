36988dd76e23e3d57b12d41bcead9d73e1931633
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-07 17:08:27 -0400

    Create countHi2.java

---
+++ CodingBat Solutions/Recursion-1/countHi2.java
@@ -0,0 +1,12 @@
+public int countHi2(String str) {
+    if(str.length() <= 1)
+        return 0;
+          
+    if(str.length() > 2 && str.substring(0, 3).equals("xhi"))
+        return countHi2(str.substring(3));
+                    
+    if(str.substring(0, 2).equals("hi"))
+        return 1 + countHi2(str.substring(2));
+                            
+    return countHi2(str.substring(1));
+}
