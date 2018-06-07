03077dc7963fd54c3f5ed2b87008848f7823b4a8
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-07 17:53:15 -0400

    Create  strDist.java

---
+++ CodingBat Solutions/Recursion-1/strDist.java
@@ -0,0 +1,13 @@
+public int strDist(String str, String sub) {
+    if(str.length() < sub.length())
+        return 0;
+          
+    if(str.substring(0, sub.length()).equals(sub) &&
+        str.substring(str.length() - sub.length()).equals(sub))
+        return str.length();
+                        
+    if(!str.substring(0, sub.length()).equals(sub))
+        return strDist(str.substring(1), sub);
+                                  
+    return strDist(str.substring(0, str.length()-1), sub);
+}
