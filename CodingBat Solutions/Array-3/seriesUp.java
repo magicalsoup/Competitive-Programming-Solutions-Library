98bad618411142050e494a16160267c15572a027
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-08 19:00:02 -0400

    Create seriesUp.java

---
+++ CodingBat Solutions/Array-3/seriesUp.java
@@ -0,0 +1,14 @@
+public int[] seriesUp(int n) {
+    int[] arr = new int[n*(n+1)/2];
+    
+    int index = 0;
+        
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j < i; j++) {
+            arr[index + j] = j + 1;
+        }
+        index += i;
+    }
+                                
+    return arr;
+}
