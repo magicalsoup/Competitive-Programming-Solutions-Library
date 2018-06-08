8f7f8f134c7632c4fc28cf6b645d39ef79c5f7fc
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-08 19:58:58 -0400

    Update changeXY.java

--- CodingBat Solutions/Recursion-1/changeXY.java
+++ CodingBat Solutions/Recursion-1/changeXY.java
@@ -1,4 +1,9 @@
 public String changeXY(String str) {
-  str = str.replaceAll("x","y");
-  return str;
+    if(str.length() == 0)
+        return str;
+          
+    if(str.charAt(0) == 'x')
+        return 'y' + changeXY(str.substring(1));
+                    
+    return str.charAt(0) + changeXY(str.substring(1));
 }
