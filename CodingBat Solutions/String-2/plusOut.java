01e045a0ba9f9b1c53411d40c92771f310a36781
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-08 15:57:38 -0400

    Create plusOut.java

---
+++ CodingBat Solutions/String-2/plusOut.java
@@ -0,0 +1,17 @@
+public String plusOut(String str, String word) {
+    StringBuffer result = new StringBuffer();
+    int i = 0;
+      
+    while(i < str.length()) {
+        if(i <= str.length() - word.length() && 
+            str.substring(i, i + word.length()).equals(word)) {
+            result.append(word);
+            i += word.length();
+        } else {
+            result.append("+");
+            i++;
+        }
+    }
+ 
+    return result.toString();
+}
