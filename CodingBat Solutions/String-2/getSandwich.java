09a61817fd0145fb73fb7723dd1ad1334210ceb1
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-08 14:54:00 -0400

    Create getSandwich.java

---
+++ CodingBat Solutions/String-2/getSandwich.java
@@ -0,0 +1,23 @@
+public String getSandwich(String str) {
+    int first = -1;
+    int last = -1;
+      
+    for(int i = 0; i < str.length() - 5; i++) {
+        if(str.substring(i, i + 5).equals("bread")) {
+            first = i;
+            break;
+        }
+    }
+                                
+    for(int i = str.length() - 5; i >= 0; i--) {
+        if(str.substring(i, i + 5).equals("bread")) {
+            last = i;
+            break;
+        }
+    }
+                                                          
+    if(first != -1 && last != -1 && first != last)
+        return str.substring(first + 5, last);
+                                                                    
+    return "";
+}
