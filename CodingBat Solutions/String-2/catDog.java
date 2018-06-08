94598ecc7ee9037f09e978b1f189340cdf4f30b4
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-07 21:25:39 -0400

    Create catDog.java

---
+++ CodingBat Solutions/String-2/catDog.java
@@ -0,0 +1,11 @@
+public boolean catDog(String str) {
+  int cat =0 ;
+  int dog =0 ;
+  for(int i=0;i<str.length()-2;i++){
+    if(str.substring(i,i+3).equals("cat"))
+    cat++;
+    if(str.substring(i,i+3).equals("dog"))
+    dog++;
+  }
+  return cat==dog;
+}
