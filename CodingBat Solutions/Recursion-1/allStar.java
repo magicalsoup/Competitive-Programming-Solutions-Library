4ff2412c337d9e10d4639f2c6e4c0af7eb0b2241
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-08 20:06:24 -0400

    Update allStar.java

--- CodingBat Solutions/Recursion-1/allStar.java
+++ CodingBat Solutions/Recursion-1/allStar.java
@@ -1,12 +1,5 @@
 public String allStar(String str) {
- 	if(str.length()<=0){
- 	 return "";
- 	}
- 	if(str.length()-1<=0) {
-str = str.substring(str.length()-1, str.length());
-return str;
-}
-String t = str.substring(str.length()-1, str.length());
-str = str.substring(0, str.length()-1);
-return allStar(str)+"*"+t;
+    if(str.length() <= 1)
+        return str;
+    return str.charAt(0) + "*" + allStar(str.substring(1));
 }
