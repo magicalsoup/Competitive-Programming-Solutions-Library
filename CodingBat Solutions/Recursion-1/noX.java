c1ceab35ef0017472f6ab5ec380e1cca51ac32b2
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-08 20:02:28 -0400

    Update noX.java

--- CodingBat Solutions/Recursion-1/noX.java
+++ CodingBat Solutions/Recursion-1/noX.java
@@ -1,4 +1,7 @@
 public String noX(String str) {
-  str = str.replaceAll("x","");
+if(str.length()==0)
   return str;
+  if(str.substring(0,1).equals("x"))
+  return ""+noX(str.substring(1));
+  return str.charAt(0)+noX(str.substring(1));
 }
