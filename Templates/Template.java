8856ef1ec8c6c9e4be3d86f7c3113a6dc1170221
James <39444003+magicalsoup@users.noreply.github.com>
2018-06-22 21:53:18 -0400

    Update Template.java

--- Templates/Template.java
+++ Templates/Template.java
@@ -8,24 +8,25 @@
       
      
       }
-    static String next () throws IOException {
-    while (st == null || !st.hasMoreTokens())
-      st = new StringTokenizer(br.readLine().trim());
-    return st.nextToken();
-     }
-    static long readLong () throws IOException {
-      return Long.parseLong(next());
-     }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
 
-     static int readInt () throws IOException {
-      return Integer.parseInt(next());
-     }
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
 
-     static double readDouble () throws IOException {
-      return Double.parseDouble(next());
-     }
-
-     static String readLine () throws IOException {
-      return br.readLine().trim();
-     } 
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
   }
