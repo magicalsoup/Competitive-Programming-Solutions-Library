db9a979410441bd1d66a3c7af8ac5b24ae18ed94
James <39444003+magicalsoup@users.noreply.github.com>
2018-09-13 21:22:05 -0400

    Create set.java

---
+++ DMOJ Solutions/set.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class set {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int n = sc.nextInt();
+		while(n-->0) {
+			int x = sc.nextInt();
+			if(!list.contains(x))
+				list.add(x);
+		}
+		System.out.println(list.size());
+		sc.close();
+	}
+}
