5e9f261b643dcf60a6fcbc14195e0fa2afd09eaf
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-02 23:44:12 -0400

    Create ccc18j2.java

---
+++ CCC Stage 1 Solutions/ccc18j2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class ccc18j2{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt(), cnt = 0; String s1 = next(), s2 = next();
+    	  for(int i = 0; i < n; i++) if(s1.charAt(i) == 'C' && s2.charAt(i) == 'C') cnt++;
+    	  pw.println(cnt); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
