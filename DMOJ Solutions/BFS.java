0138f7fab3bfd2a8e05a90c1c3277c03102c33dd
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-03 17:01:36 -0400

    Create BFS.java

---
+++ DMOJ Solutions/BFS.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+    public class BFS{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt() * 5 + readInt() * 10 + readInt() * 25 + readInt() * 100 + readInt() * 200;
+    	  pw.println(n); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
