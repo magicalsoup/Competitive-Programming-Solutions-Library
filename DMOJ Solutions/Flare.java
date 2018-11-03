1f52375b21f93ff678d4a6b0f190160360f14fff
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-03 15:48:17 -0400

    Create Flare.java

---
+++ DMOJ Solutions/Flare.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+    public class Flare{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	 double t = readInt(); pw.println(t/4.9); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
