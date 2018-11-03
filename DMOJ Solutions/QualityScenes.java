5c2a2eb437b17f5858df4b0ed0812f7e325d8b62
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-03 16:27:09 -0400

    Create QualityScenes.java

---
+++ DMOJ Solutions/QualityScenes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class QualityScenes{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int a = readInt(), b = readInt(), c = readInt(), d = readInt();
+    	  if( (c < b && c >= a) || (a >= c &&  a < d)) pw.println("YES"); else pw.println("NO");
+    	  pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
