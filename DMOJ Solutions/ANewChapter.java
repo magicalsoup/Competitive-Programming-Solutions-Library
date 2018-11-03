155cfa0169df7f375ee6f3c92eccd3329a58a7bf
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-03 17:38:30 -0400

    Create ANewChapter.java

---
+++ DMOJ Solutions/ANewChapter.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+    public class ANewChapter{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt(), cnt = 0; for(int i = 0; i < n; i++) if(readInt()%2 == i%2) cnt++;
+    	  pw.println(cnt); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
