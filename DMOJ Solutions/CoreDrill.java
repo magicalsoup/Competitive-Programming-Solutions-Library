2f5e89a6f7ebd1802efe486e1f8a34fd1b0b7e1a
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-03 12:18:18 -0400

    Create CoreDrill.java

---
+++ DMOJ Solutions/CoreDrill.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+    public class CoreDrill{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	 double r = readDouble(), h = readDouble(), pi = Math.PI; r *= r;
+    	 pw.println((r*pi*h)/3.0); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
