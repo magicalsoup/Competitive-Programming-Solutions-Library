f9bcf8f536c8bc9efc5b174e6c70af42e4b46d69
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-02 23:55:15 -0400

    Create Tiles.java

---
+++ DMOJ Solutions/Tiles.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+    public class Tiles{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	 int w = readInt(), l = readInt(), square = readInt(), w1 = w / square, w2 = l / square;
+    	 pw.println(w1 * w2); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
