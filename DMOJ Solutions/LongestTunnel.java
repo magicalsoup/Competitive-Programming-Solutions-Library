ae7db0160f53c6548e11f56a72df6f8d063ab050
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-03 12:20:50 -0400

    Create LongestTunnel.java

---
+++ DMOJ Solutions/LongestTunnel.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class LongestTunnel{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	 int n = readInt(), max = Integer.MIN_VALUE;
+    	 for(int i = 0; i < n; i++) max = Math.max(max, Math.abs(readInt() - readInt()));
+    	 pw.println(max); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
