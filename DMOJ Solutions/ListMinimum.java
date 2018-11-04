41f2f4c7e2db6e22deb269c9b089556d1eb024ae
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-04 00:06:00 -0400

    Create ListMinimum.java

---
+++ DMOJ Solutions/ListMinimum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+    public class ListMinimum{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt(); for(int i = 1; i <= n; i++) pw.print(i + " "); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
