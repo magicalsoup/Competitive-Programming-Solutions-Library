9291050431dce3b015a0c6b0c62cfc6cfcc9fbde
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-03 23:59:25 -0400

    Create MockingSpongeBob.java

---
+++ DMOJ Solutions/MockingSpongeBob.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+    public class MockingSpongeBob{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt();
+    	  while(n-- > 0) {
+    		  String s = readLine(); int index = -1;
+    		  for(int i = 0; i < s.length(); i++) {
+    			  if(index % 2 != 0 || index == -1)  System.out.print(s.substring(i, i + 1).toLowerCase());
+    			  else System.out.print(s.substring(i, i + 1).toUpperCase());
+    			  if(Character.isAlphabetic(s.charAt(i))) index++;
+    		  }
+    		  System.out.println();
+    	  }
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
