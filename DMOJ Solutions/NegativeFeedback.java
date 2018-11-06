21c5450340ecce0117133a3f97f3bac73420cee3
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-05 20:31:29 -0500

    Create NegativeFeedback.java

---
+++ DMOJ Solutions/NegativeFeedback.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class NegativeFeedback{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      static int s;
+      public static void main(String[]args) throws IOException{
+    	  double Vin = readDouble(), Rf = readDouble(), Rg = readDouble();
+    	  double Vout = Vin * (1 + Rf/Rg); pw.println(Vout); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
