361f9748a510317302205ce7e9993407ea0bc767
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-05 22:34:08 -0500

    Create Satisfiability.java

---
+++ DMOJ Solutions/Satisfiability.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+    public class Satisfiability{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  double b = readDouble(); double ans = b / 2; pw.printf("$%.2f $%.2f", ans, ans); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+       static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+       static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
