68926c21e843bb3d4a698ef228722c0240ba14f9
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-06 12:32:59 -0500

    Create nccc5j1.java

---
+++ CCC Stage 1 Solutions/nccc5j1.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+   public class nccc5j1{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  String a = next(); pw.println(a.contains("CCC")? "NO" :"YES"); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+   }
