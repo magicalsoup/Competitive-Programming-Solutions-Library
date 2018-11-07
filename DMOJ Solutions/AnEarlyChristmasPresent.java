840d80b1dfa2b61ed206d43dd81efe02dcf02015
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-06 19:16:38 -0500

    Create AnEarlyChristmasPresent.java

---
+++ DMOJ Solutions/AnEarlyChristmasPresent.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class AnEarlyChristmasPresent{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt(); int min = Integer.MAX_VALUE;
+    	  for(int i = 0; i < n; i++) min = Math.min(min, readInt());
+    	  pw.println(min); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
