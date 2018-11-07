a67bdc832b74628e0bca4111b7c5c8831bded73f
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-06 19:34:52 -0500

    Create PizzaOrder.java

---
+++ DMOJ Solutions/PizzaOrder.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+    public class PizzaOrder{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  double p = Math.ceil(readInt() / 3.0), c = Math.ceil(readInt() / 3.0), v = Math.ceil(readInt() / 3.0);
+    	  pw.println((int)(p + c + v)); pw.close();
+      } 
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
