4c69e896a6c5213d53c4e0e1f20e2ed44b5674a6
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-06 19:08:42 -0500

    Update nccc1j2.java

--- CCC Stage 1 Solutions/nccc1j2.java
+++ CCC Stage 1 Solutions/nccc1j2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class ASubstringProblem {
+public class nccc1j2 {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		String s = sc.next();
