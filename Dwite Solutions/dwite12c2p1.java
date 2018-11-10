249a6403758a66eeda9e336a9b0458b170e8c4bd
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-10 13:19:28 -0500

    Create dwite12c2p1.java

---
+++ Dwite Solutions/dwite12c2p1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+    public class dwite12c2p1{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  for(int t = 0; t < 5; t++) {
+    		  int n = readInt(), c = readInt(), time = 0, cnt = 0, rate = 1;
+    		  while(n  > 0) {int x = Math.min(n, rate); n -= x; cnt += x; if(cnt >= c) {rate *= 2; cnt = 0;} time++;}
+    		  pw.println(time);
+    	  }
+    	  pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
