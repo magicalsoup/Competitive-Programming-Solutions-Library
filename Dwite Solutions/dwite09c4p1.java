557c07d6ffafd760ae79314175749c048986221b
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-10 16:32:19 -0500

    Create dwite09c4p1.java

---
+++ Dwite Solutions/dwite09c4p1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+    public class dwite09c4p1{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  for(int t = 0; t < 5; t++) {
+    		  double a = readDouble(); double ans = 150 / a;
+    		  pw.println(Math.round(ans * 60));
+    	  }
+    	  pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
