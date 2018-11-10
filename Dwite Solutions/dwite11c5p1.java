76f3a3d6932443da4ae03971c8bc898b52ba10f7
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-10 15:51:38 -0500

    Create dwite11c5p1.java

---
+++ Dwite Solutions/dwite11c5p1.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+    public class dwite11c5p1{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  for(int t = 0; t < 5; t++) {
+    		  int day = readInt(), month = readInt(), year = readInt();
+    		  boolean flag = year < 1997 || year == 1997 && month < 10 || year == 1997 && month == 10 && day == 27;
+    		  pw.println(flag? "old enough" : "too young");
+    	  }
+    	  pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
