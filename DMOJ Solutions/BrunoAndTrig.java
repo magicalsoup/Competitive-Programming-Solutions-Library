9f8032560a6a73c83246066328a32c9eb8ee60e8
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-21 16:31:40 -0500

    Create BrunoAndTrig.java

---
+++ DMOJ Solutions/BrunoAndTrig.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class BrunoAndTrig{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int a = readInt(), b = readInt(), c = readInt();
+    	  if(a + b > c && b + c > a && c + a > b) pw.println("Huh? A triangle?");
+    	  else pw.println("Maybe I should go out to sea..."); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
