7b98cfb33f475b932218265f130ddadd74a9a4c5
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-23 13:28:58 -0500

    Create PurchasingPresents.java

---
+++ DMOJ Solutions/PurchasingPresents.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+    public class PurchasingPresents{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt(); double money = readDouble(), price = 0.0;
+    	  for(int i = 0; i < n ;i++) price += readDouble();
+    	  if(price > money) pw.println("Fardin's broke");
+    	  else pw.printf("%.2f\n", money - price); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
