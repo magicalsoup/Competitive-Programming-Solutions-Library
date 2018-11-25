e93e2c18b7dd4dca6bc0c01ad6260446e745a69a
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-25 13:53:50 -0500

    Create ObservingFrogs.java

---
+++ DMOJ Solutions/ObservingFrogs.java
@@ -0,0 +1,31 @@
+ import java.util.*;
+ import java.io.*;
+    public class ObservingFrogs{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  long n = readLong(); pw.println(n * n / 2); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
