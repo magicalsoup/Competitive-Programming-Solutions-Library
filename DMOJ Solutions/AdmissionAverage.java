8851f7bc4b6068fda34df5219396fe2f4718c877
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-25 22:44:07 -0500

    Create AdmissionAverage.java

---
+++ DMOJ Solutions/AdmissionAverage.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+    public class AdmissionAverage{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  double cnt = 0; for(int i = 0; i < 6; i++) cnt += readDouble(); cnt /= 6; cnt += readDouble();
+    	  int min = readInt(); if(cnt >= min) pw.println("yes"); else pw.println("no"); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
