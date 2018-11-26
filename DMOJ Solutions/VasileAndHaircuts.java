11150eaa9a9bb5229a5bfbe586103b3c52e31cf6
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-25 23:00:34 -0500

    Create VasileAndHaircuts.java

---
+++ DMOJ Solutions/VasileAndHaircuts.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class VasileAndHaircuts{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int n = readInt();
+    	  while(n-- >0) {int l = readInt(), a = l - readInt(), b = l - readInt(), f = readInt(); pw.println(f >= b && f <= a? "Yes" : "No");}
+    	  pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
