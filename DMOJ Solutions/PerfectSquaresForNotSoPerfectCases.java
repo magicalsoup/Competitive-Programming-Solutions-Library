7157cffb7152d4539b4b4013d296029f98eb4022
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-26 19:02:18 -0500

    Create PerfectSquaresForNotSoPerfectCases.java

---
+++ DMOJ Solutions/PerfectSquaresForNotSoPerfectCases.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class PerfectSquaresForNotSoPerfectCases{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int a = readInt(), b = readInt(), cnt = 0;
+    	  for(int i = 0; i * i <= b; i++) if(i * i >= a) cnt++;
+    	  pw.println(cnt); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
