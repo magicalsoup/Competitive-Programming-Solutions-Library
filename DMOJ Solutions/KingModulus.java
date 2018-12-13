c7d88850e4e0777dedc67e137e3559f62a11a0f8
James <39444003+magicalsoup@users.noreply.github.com>
2018-12-13 16:01:55 -0500

    Create KingModulus.java

---
+++ DMOJ Solutions/KingModulus.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+    public class KingModulus{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int a = readInt(), b = readInt(); while(a < b) a += b; pw.println(a % b); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
