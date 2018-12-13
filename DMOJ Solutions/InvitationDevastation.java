84c77f0956c0a7ddbc8d1757824d3df7f71a6ecd
James <39444003+magicalsoup@users.noreply.github.com>
2018-12-13 08:20:04 -0500

    Create InvitationDevastation.java

---
+++ DMOJ Solutions/InvitationDevastation.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+    public class InvitationDevastation{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  String s = readLine(); int n = readInt(); for(int i = 0; i < n; i++) {String t = next(); pw.println(s.replaceAll(">", t)); }pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
