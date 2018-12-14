d80f039fd4c280c437be9375dbb5bfe1e2d6b6a9
James <39444003+magicalsoup@users.noreply.github.com>
2018-12-13 22:36:40 -0500

    Create DrHenriAndDifferentialPhotometry.java

---
+++ DMOJ Solutions/DrHenriAndDifferentialPhotometry.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+    public class DrHenriAndDifferentialPhotometry{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int k = readInt(); double avg = 0.0, mx = readDouble();
+    	  for(int i = 0; i < k; i++) { int n = readInt(); avg += mx; while(n-- > 0) avg += readDouble();}
+    	  pw.println(avg / k); pw.close();
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
