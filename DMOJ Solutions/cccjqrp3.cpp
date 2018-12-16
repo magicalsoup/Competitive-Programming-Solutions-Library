1bb5d07ba98a552007c077452ccd1125b88fac04
James <39444003+magicalsoup@users.noreply.github.com>
2018-12-16 14:20:05 -0500

    Create cccjqrp3.cpp

---
+++ DMOJ Solutions/cccjqrp3.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+unsigned long long a, b, c;
+unsigned long long mod = 42069900169420;
+int main() {
+    cin>>a>>b>>c;
+    cout<<(a + b + c) % mod;
+    return 0;
+}
