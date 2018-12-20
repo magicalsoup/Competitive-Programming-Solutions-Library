56db0fee614ba2156803928f2a4fb6df186e135e
James <39444003+magicalsoup@users.noreply.github.com>
2018-12-20 12:03:58 -0500

    Create RoundingErrors.java

---
+++ DMOJ Solutions/RoundingErrors.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class RoundingErrors{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+    static StringTokenizer st;
+    public static void main(String[]args) throws IOException{
+    	int a = readInt(), up = 0, down = 0;
+    	while(a-- > 0){double d = readDouble() % 1; if(d >= 0.5) up++; else down++;}
+    	pw.println(up); pw.println(down); pw.close();
+    }
+	static String next () throws IOException {
+  	  while (st == null || !st.hasMoreTokens())
+  		  st = new StringTokenizer(br.readLine().trim());
+  	  return st.nextToken();
+    }
+    
+    static long readLong () throws IOException {
+  	  return Long.parseLong(next());
+    }
+
+    static int readInt () throws IOException {
+  	  return Integer.parseInt(next());
+    }
+
+    static double readDouble () throws IOException {
+  	  return Double.parseDouble(next());
+    }
+    
+    static String readLine () throws IOException {
+  	  return br.readLine().trim();
+    } 
+}
