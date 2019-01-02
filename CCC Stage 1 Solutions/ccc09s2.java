7a9e256323a4ce2be79fb54e45c6b793a83831b6
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-01 22:48:52 -0500

    Update ccc09s2.java

--- CCC Stage 1 Solutions/ccc09s2.java
+++ CCC Stage 1 Solutions/ccc09s2.java
@@ -1,54 +1,47 @@
 import java.util.*;
-public class ccc09s2{
-public static void main(String[]args) {
-	Scanner scan = new Scanner(System.in);
-	String [] rows;
-	String [] above = new String[256];
-	int abovesize;
-	String[] below = new String[256];
-	int belowsize;
-	int r,l,k;
-	String newrow;
-	r = scan.nextInt();
-	l = scan.nextInt();
-	rows = new String[r];
-	for(int i=0;i<r;i++)
-	{
-		rows[i] = "";
-		for(int j=0;j<l;j++) {
-			rows[i] = rows[i]+ scan.next();
-		}
-	}
-	above[0] = rows[0];
-	abovesize = 1;
-	belowsize = 1;
-	for(int i=1;i<r;i++) {
-		below[0]= rows[i];
-		belowsize = 1;
-		for(int j=0;j<abovesize;j++) {
-			newrow = pushButton(above[j],below[0]);
-			k =0;
-			while(k<belowsize&&!below[k].equals(newrow))
-				k++;
-			if(k>=belowsize) {
-				below[belowsize] = newrow;
-				belowsize++;
-			}
-		}
-		for(int j=0;j<belowsize;j++)
-			above[j] =below[j];
-		abovesize = belowsize;
-	}
-	System.out.println(belowsize);
-}
-public static String pushButton(String s,String t) {
-	String x;
-	x ="";
-	for(int i=0;i<s.length();i++)
-		if(s.charAt(i)==t.charAt(i))
-			x= x+"0";
-		else
-			x= x+'1';
-	return x;
-}
-}
+import java.io.*;
+    public class ccc09s2{
+      static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
+      static StringTokenizer st;
+      public static void main(String[]args) throws IOException{
+    	  int r = readInt(), l = readInt(); String lights[] = new String[r];
+    	  for(int i = 0; i < r; i++)  lights[i] = readLine().replaceAll(" ", "");
+    	  Set<String> set = new HashSet<String>();
+    	  for(int i = 0; i < r; i++) {
+    		  String res = lights[i];
+    		  for(int j = i + 1; j < r; j++) res = pushButton(res, lights[j]);
+    		  set.add(res);
+    	  }
+    	  pw.println(set.size()); pw.close();
+      }	
+      static String pushButton(String a, String b) {
+    	  String res = "";
+    	  for(int i = 0; i < a.length(); i++) {
+    		  	if(a.charAt(i) == b.charAt(i)) res += 0;
+    		  	else res += 1;
+    	  }
+    	  return res;
+      }
+      static String next () throws IOException {
+    	  while (st == null || !st.hasMoreTokens())
+    		  st = new StringTokenizer(br.readLine().trim());
+    	  return st.nextToken();
+      }
+      
+      static long readLong () throws IOException {
+    	  return Long.parseLong(next());
+      }
+
+      static int readInt () throws IOException {
+    	  return Integer.parseInt(next());
+      }
+
+      static double readDouble () throws IOException {
+    	  return Double.parseDouble(next());
+      }
+      
+      static String readLine () throws IOException {
+    	  return br.readLine().trim();
+      } 
+  }
