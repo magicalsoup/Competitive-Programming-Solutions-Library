9fc019d42c07d6705a8e5f26d2286db21e83ddc2
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-02 20:17:04 -0500

    Update TestScores.java

--- DMOJ Solutions/TestScores.java
+++ DMOJ Solutions/TestScores.java
@@ -5,27 +5,27 @@ import java.io.*;
       static PrintWriter pw = new PrintWriter(new OutputStreamWriter(System.out));
       static StringTokenizer st;
       public static void main(String[] args) throws Exception{
-    	  int n = readInt(), m = readInt(), k = readInt(); long cnt = 0; Test t[] = new Test[n];
-    	  for(int i = 0; i < n; i++) { t[i] = new Test(readInt(), readInt()); cnt += t[i].score;}
+    	  int n = readInt(); long m = readLong(), k = readLong(); long cnt = 0; Test t[] = new Test[n];
+    	  for(int i = 0; i < n; i++) { t[i] = new Test(readLong(), readLong()); cnt += t[i].score;}
     	  if(cnt >= n * k) { pw.println(0); pw.close(); return;} long ans = 0; Arrays.sort(t);
     	  for(int i = 0; i < n; i++) {
     		  if(cnt + (m - t[i].score) <= n * k) {
     			  ans += (m - t[i].score) * t[i].hour;
-    			  cnt += m - t[i].score;
+    			  cnt += (m - t[i].score);
     		  }
-    		  else { ans += (n * k - cnt) * t[i].hour; break;}
+    		  else { ans += t[i].hour * (n * k - cnt) ; break;}
     	  }
     	  pw.println(ans); pw.close();
       }
       static class Test implements Comparable<Test>{
-    	  int score, hour;
-    	  public Test(int score, int hour) {
+    	  long score, hour;
+    	  public Test(long score, long hour) {
     		  this.score = score; this.hour = hour;
     	  }
     	  @Override
     	  public int compareTo(Test t) {
-    		  if(t.hour == hour) return t.score - score;
-    		  return hour - t.hour;
+    		  if(t.hour == hour) return Long.compare(t.score, score);
+    		  return Long.compare(hour, t.hour);
     	  }
       }
       static String next () throws IOException {
