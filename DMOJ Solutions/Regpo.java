21cd56041ca18af597b222b0a3d9551705edd060
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-06 18:46:40 -0500

    Create Regpo.java

---
+++ DMOJ Solutions/Regpo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+    public class Regpo{
+      public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+    	  double v = sc.nextInt();
+    	  double c = 360 / v;
+    	  System.out.printf("%.6f", c); 
+    	  sc.close();
+      }
+  }
