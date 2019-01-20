4e6286817f9c894b714e6aa872f9f26796df11b4
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-20 17:56:39 -0500

    Update ccc00s4.java

--- CCC Stage 1 Solutions/ccc00s4.java
+++ CCC Stage 1 Solutions/ccc00s4.java
@@ -1,25 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class ccc00s4{
 	public static void main (String [] args){
-    	Scanner sc = new Scanner(System.in);
-	int d = sc.nextInt();
-	int c = sc.nextInt();
-	int arr[] = new int[c];//c
-	int arr2[] = new int[d+1];//d
-	arr2[0] = 0;
-	for(int i=0;i<c;i++)
-		arr[i] = sc.nextInt();
-	for(int i=1;i<=d;i++)
-	{
-		arr2[i] = Integer.MAX_VALUE-1;
-		for(int j=0;j<c;j++)
-			if(arr[j]<=i)
-				arr2[i] = Math.min(arr2[i], (arr2[i-arr[j]]+1));
-	}
-	if(arr2[d]<Integer.MAX_VALUE-1)
-		System.out.println("Roberta wins in "+arr2[d]+" strokes." );
-	else
-		System.out.println("Roberta acknowledges defeat.");
+		Scanner sc = new Scanner(System.in);
+		int d = sc.nextInt(), n = sc.nextInt(); int dp[] = new int[d + 1], v[] = new int[n];
+		for(int i = 0; i< n; i++) v[i] = sc.nextInt();
+		dp[0] = 0; Arrays.fill(dp, 0x3f3f3f3f);
+		for(int i = 0; i < n; i++) {
+			for(int j = 1; j <= d; j++) {
+				if(v[i] <= j)
+					dp[j] = Math.min(dp[j], dp[j - v[i]] + 1);
+			}
+		}
+		if(dp[d] == 0x3f3f3f3f) System.out.println("Roberta acknowledges defeat.");
+		else System.out.println("Roberta wins in " + dp[d] + " strokes.");
+		sc.close();
 	}
 }
 
