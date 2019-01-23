1a5b3153d9bad52c7cf00794923054cda89530a8
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-01-22 22:02:17 -0500

    Create aplusb.java

---
+++ SSOJ Solutions/aplusb.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class aplusb {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		while(n-- >0) {
+			System.out.println(sc.nextLong() + sc.nextLong());
+		}
+		sc.close();
+	}
+}
