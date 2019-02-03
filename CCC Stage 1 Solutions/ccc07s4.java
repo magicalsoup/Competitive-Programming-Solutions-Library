b1d70e3f18238413a55fc61e196c1eb29d4d4a4c
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-02-03 14:09:36 -0500

    Update ccc07s4.java

--- CCC Stage 1 Solutions/ccc07s4.java
+++ CCC Stage 1 Solutions/ccc07s4.java
@@ -1,52 +1,24 @@
 import java.util.*;
 public class ccc07s4{
-	static int n;
-	static int [] paths;
-	static int numpaths =0;
-	static int[] startingpts;
-	static int[] endingpts;
+	static ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+	static int dp[];
 	public static void main(String[]args) {
-	Scanner scan = new Scanner(System.in);
-	n = scan.nextInt();
-	paths = new int[n+1];
-	for(int i=0;i<n+1;i++)
-		paths[i] = -1;
-	numpaths =0;
-	int x = scan.nextInt();
-	int y = scan.nextInt();
-	startingpts = new int[99999];
-	endingpts = new int[99999];
-	numpaths  =0;
-	while(!(x==0&&y==0))
-	{
-		startingpts [numpaths] = x;
-		endingpts[numpaths] = y;
-		numpaths++;
-		x = scan.nextInt();
-		y = scan.nextInt();
-	}
-	System.out.println(find(1));
-	}
-	public static int find(int pt) {
-		if(pt==n)
-			return 1;
-		else {
-			int sum =0;
-			for(int i=0;i<numpaths;i++)
-			{
-				if(startingpts[i] ==pt) {
-					int ans =paths[endingpts[i]];
-					if(ans!=-1)
-						sum+=ans;
-					else
-					{
-						int a =find(endingpts[i]);
-						paths[endingpts[i]]=a;
-						sum+=a;
-					}
-				}
-			}
-			return sum;
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(); dp = new int[n + 1];
+		for(int i = 0; i <= n; i++) adj.add(new ArrayList<Integer>());
+		while(true) {
+			int x = sc.nextInt(), y = sc.nextInt();
+			if(x == 0 && y == 0) break;
+			adj.get(x).add(y);
 		}
+		dp[1] = 1;
+		for(int i = 1; i <= n; i++) {
+			for(int j = 0; j < adj.get(i).size(); j++) {
+				int next = adj.get(i).get(j);
+				dp[next] += dp[i];
+			}
+		}
+		System.out.println(dp[n]);
+		sc.close();
 	}
 }
