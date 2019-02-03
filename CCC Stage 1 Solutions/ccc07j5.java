cd61b81005211441f39aa35f26517cf4273862a5
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-02-03 13:41:43 -0500

    Update ccc07j5.java

--- CCC Stage 1 Solutions/ccc07j5.java
+++ CCC Stage 1 Solutions/ccc07j5.java
@@ -1,50 +1,20 @@
 import java.util.*;
 public class ccc07j5{
-	static 	int motel[];
-	static int ways =0;
-	static int min,max,size, n;
-		public static boolean between(int a,int b, int c) {
-			return  a<=b&&b<=c;
-		}
-		public static int findpaths(int distance) {
-			if(distance==7000) {
-				ways++;
+	static List<Integer> pos = new ArrayList<Integer>(Arrays.asList(0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000));
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt(), b = sc.nextInt(), m = sc.nextInt();
+		for(int i = 0; i < m; i++) pos.add(sc.nextInt());
+		long dp[] = new long[pos.size()]; dp[0] = 1;
+		Collections.sort(pos);
+		for(int i = 1; i < pos.size(); i++) {
+			for(int j = 0; j < i; j++) {
+				int dis = pos.get(i) - pos.get(j);
+				if(dis >= a && dis <= b) dp[i] += dp[j];
 			}
-			else {
-				for(int i=1;i<=size;i++) {
-					if(between(min,motel[i]-distance,max)) {
-						findpaths(motel[i]);
-					}
-				}
-			}
-			return ways;
 		}
-public static void main(String[]args) {
-	Scanner sc= new Scanner(System.in);
-	size = 13;
-	motel = new int[50];
-	motel[1] = 990;
-	motel[2] = 1010;
-	motel[3] = 1970;
-	motel[4] = 2030;
-	motel[5] = 2940;
-	motel[6] = 3060;
-	motel[7] = 3930;
-	motel[8] = 4060;
-	motel[9] = 4970;
-	motel[10] = 5030;
-	motel[11] = 5990;
-	motel[12] = 6010;
-	motel[13] = 7000;
-	min = sc.nextInt();
-	max = sc.nextInt();
-	n = sc.nextInt();
-	for(int i=1;i<=n;i++) {
-		motel[i+size] = sc.nextInt();
+		System.out.println(dp[pos.size() - 1]);
+		sc.close();
 	}
-	size = size+n;
-	findpaths(0);
-	System.out.print(ways);
-}
 }
 
