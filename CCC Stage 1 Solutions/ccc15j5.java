a33f79eeba5291aaa97479600d7aa49128a71ac6
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-02-03 12:34:37 -0500

    Update ccc15j5.java

--- CCC Stage 1 Solutions/ccc15j5.java
+++ CCC Stage 1 Solutions/ccc15j5.java
@@ -1,27 +1,17 @@
 import java.util.*;
 public class ccc15j5{
-	static int[][][] dp = new int[250][250][250];
-	public static int pi(int n,int k, int min) {
-		if(n==k||k==1)return 1;
-		if(!(n>0&&k>0)||k>n)return 0;
-		int count =0;
-		int temp;
-		for(int i = min;i<=n/k;i++) {
-			if(dp[n-i][k-1][i]==0) {
-				temp = pi(n-i,k-1,i);
-				count+=temp;
-				dp[n-i][k-1][i] = temp;
-			}
-			else {
-				count+=dp[n-i][k-1][i];
-			}
-		}
-		return count;
+	static int[][] dp = new int[250][250];
+	public static int pi(int n, int k) {
+		if(n < 0 || (k==0 && n != 0)) return 0;
+		if(n == 0 && k != 0) return 1;
+		if(dp[n][k] != 0) return dp[n][k];
+		return dp[n][k] = pi(n, k - 1) + pi(n - k, k);
 	}
-public static void main(String[]args) {
-	Scanner scan = new Scanner(System.in);
-	int n = scan.nextInt();
-	int k= scan.nextInt();
-	System.out.println(pi(n,k,1));
-}
+	public static void main(String[]args) {
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int k = scan.nextInt();
+		System.out.println(pi(n - k, k));
+	}
 }
