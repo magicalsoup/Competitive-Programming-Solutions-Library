f8327889a0c2bbc213c30c7edd76ccc3cb35b697
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-02-10 13:10:35 -0500

    Update mockccc15j5.java

--- CCC Stage 1 Solutions/mockccc15j5.java
+++ CCC Stage 1 Solutions/mockccc15j5.java
@@ -1,92 +1,36 @@
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-
+import java.util.*;
+import java.io.*;
 public class mockccc15j5{
 
   static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  static PrintWriter ps = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+  static PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
   static StringTokenizer st;
 
   public static void main (String[] args) throws IOException {
-    int n = readInt();
-    Map<Integer, ArrayList<Integer>> xC = new HashMap<Integer, ArrayList<Integer>>();
-    Map<Integer, ArrayList<Integer>> yC = new HashMap<Integer, ArrayList<Integer>>();
-    for (int z = 0; z < n; z++) {
-      int x = readInt();
-      int y = readInt();
-      if (xC.get(x) == null)
-        xC.put(x, new ArrayList<Integer>());
-      if (yC.get(y) == null)
-        yC.put(y, new ArrayList<Integer>());
-      xC.get(x).add(y);
-      yC.get(y).add(x);
-    }
-    for (Map.Entry<Integer, ArrayList<Integer>> e : xC.entrySet()) {
-      Collections.sort(e.getValue());
-    }
-    for (Map.Entry<Integer, ArrayList<Integer>> e : yC.entrySet()) {
-      Collections.sort(e.getValue());
-    }
-    int m = readInt();
-
-    int x = readInt();
-    int y = readInt();
-    long res = 0;
-    for (int z = 0; z < m - 1; z++) {
-      int nx = readInt();
-      int ny = readInt();
-
-      if (x == nx) {
-        if (xC.get(x) != null) {
-          Integer c1 = bs(xC.get(x), Math.max(ny, y), true);
-          Integer c2 = bs(xC.get(x), Math.min(ny, y), false);
-          res += c1 - Math.max(0, c2) + 1;
-        }
-
-      } else {
-        if (yC.get(y) != null) {
-          Integer c1 = bs(yC.get(y), Math.max(nx, x), true);
-          Integer c2 = bs(yC.get(y), Math.min(nx, x), false);
-          res += c1 - Math.max(0, c2) + 1;
-        }
-
-      }
-      x = nx;
-      y = ny;
-    }
-    System.out.println(res);
+	  int n = readInt(); long px[] = new long[n], py[] = new long[n], seed = (long)1e9, ans = 0;
+	  for(int i = 0; i < n; i++) {int x = readInt(), y = readInt(); px[i] = x * seed + y; py[i] = y * seed + x;}
+	  Arrays.sort(px); Arrays.sort(py);
+	  int m = readInt(), x0 = readInt(), y0 = readInt();
+	  for(int i = 1; i < m; i++) {
+		  int x1 = readInt(), y1 = readInt();
+		  if(y0 == y1) {
+			  int lft = Arrays.binarySearch(py, y0 * seed + Math.min(x0, x1));
+			  int rit = Arrays.binarySearch(py, y0 * seed + Math.max(x0, x1));
+			  if(lft < 0) lft = -lft - 1;
+			  if(rit < 0) rit = -rit - 2;
+			  ans += rit - lft + 1;
+		  }
+		  else {
+			  int lft = Arrays.binarySearch(px, x0 * seed + Math.min(y0, y1));
+			  int rit = Arrays.binarySearch(px, x0 * seed + Math.max(y0, y1));
+			  if(lft < 0) lft = -lft - 1;
+			  if(rit < 0) rit = -rit - 2;
+			  ans += rit - lft + 1;
+		  }
+		  x0 = x1; y0 = y1;
+	  }
+	  pw.println(ans); pw.close();
   }
-
-  private static int bs (ArrayList<Integer> a, int i, boolean lower) {
-    int lo = 0;
-    int hi = a.size() - 1;
-    while (lo <= hi) {
-      int mid = lo + (hi - lo) / 2;
-      if (lower) {
-        if (a.get(mid) <= i)
-          lo = mid + 1;
-        else
-          hi = mid - 1;
-      } else {
-        if (a.get(mid) < i)
-          lo = mid + 1;
-        else
-          hi = mid - 1;
-      }
-    }
-    return lower ? hi : lo;
-  }
-
   static String next () throws IOException {
     while (st == null || !st.hasMoreTokens())
       st = new StringTokenizer(br.readLine().trim());
