8cebd6b07f006d0063332ea0a8153fa5a2dbf02a
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-02-19 23:04:16 -0500

    Create Template2.cpp

---
+++ Templates/Template2.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define scan(x) do{while((x=getchar())<'0'); for(x-='0'; '0'<=(_=getchar()); x=(x<<3)+(x<<1)+_-'0');}while(0)
+char _;
+#define INF 0x3f3f3f3f
+#define NINF -INF
+#define MOD 1000000007
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define m(x, y) (x + y) / 2
+typedef pair<int, int> pi;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+// cannot use scanf with this template
+int main() {
+ cin.sync_with_stdio(0);
+ cin.tie(0);
+}
