b6e37e1f6953d4a31679535694ddc25b87371cd8
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-02-26 14:13:17 -0500

    Update nccc6s2.java

--- CCC Stage 1 Solutions/nccc6s2.java
+++ CCC Stage 1 Solutions/nccc6s2.java
@@ -1,11 +1,13 @@
 import java.util.*;
+import java.io.*;
 public class nccc6s2 {
-	public static void main(String[]args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(),  a[][] = new int[n + 1][n + 1];
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	public static void main(String[]args) throws IOException{
+		int n = readInt(),  a[][] = new int[n + 1][n + 1];
 		for(int i = 1; i <= n; i++)
 			for(int j = 1; j <= n; j++)
-				a[i][j] = sc.nextInt();
+				a[i][j] = readInt();
 		
 		int num = 1, cnt = 0;
 		for(int i = 1; i <= n; i++) { 
@@ -16,6 +18,14 @@ public class nccc6s2 {
 		}
 		
 		double ans = Math.ceil(Math.sqrt(cnt));
-		System.out.println((int)(ans)); sc.close();
+		System.out.println((int)(ans));
+	}
+	static String next() throws IOException{
+		while(st == null || !st.hasMoreTokens())
+			st = new StringTokenizer(br.readLine().trim());
+		return st.nextToken();
+	}
+	static int readInt() throws IOException{
+		return Integer.parseInt(next());
 	}
 }
