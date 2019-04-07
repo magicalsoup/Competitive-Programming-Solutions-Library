3ecdad3a543d6e2cd347fab279f00b8d0b1f5f9c
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-04-07 13:36:40 -0400

    Create AddOne.java

---
+++ WCICS Problems & Solutions/AddOne.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AddOne {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()+1);
+		sc.close();
+	}
+}
