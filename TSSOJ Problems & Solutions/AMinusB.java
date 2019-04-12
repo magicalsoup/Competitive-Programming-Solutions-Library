ef572c67ea88d2627864afc7ecf39e4516198351
Soup <39444003+magicalsoup@users.noreply.github.com>
2019-04-12 10:44:13 -0400

    Create AMinusB.java

---
+++ TSSOJ Problems & Solutions/AMinusB.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AMinusB {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println((sc.nextInt() - sc.nextInt()));
+		sc.close();
+	}
+}
